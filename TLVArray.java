41dd23a19178de53d1596531f64ebf8ab3238c5f
chitrendra chaudhary <32837686+chitrendrachaudhary@users.noreply.github.com>
2024-05-08 10:52:31 +0530

    Add files via upload

---
+++ TLVArray.java
@@ -0,0 +1,8 @@
+package common;
+
+public class TLVArray {
+    public byte data[] = new byte[2048];
+    //	public byte data[] = new byte[600];
+    public int len = -1;
+    public int tag;
+}
---
+++ TLVArraySort.java
@@ -0,0 +1,7 @@
+package common;
+
+public class TLVArraySort {
+    //	public byte data[] = new byte[1024];
+    public byte data[] = new byte[30];
+    public int len = -1;
+}
